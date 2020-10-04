b4235cdf2fdc689e4623803672e940bb7edd2443
aykutonen <aykutonen@gmail.com>
2020-10-04 18:50:59 +0300

    dummy onboarding sayfaları hazırlandı.

--- lib/views/onboarding_page.dart
+++ lib/views/onboarding_page.dart
@@ -1,1 +1,17 @@
-// TODO: Uygulama ilk yüklendiktan sonra açılan sayfa.
+import 'package:flutter/cupertino.dart';
+
+class Onboarding extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        child: Center(
+      child: Column(
+        children: [
+          Text('Onboarding'),
+          CupertinoButton(
+              child: Text('Go to Onboarding Setting'), onPressed: null),
+        ],
+      ),
+    ));
+  }
+}
--- lib/views/onboarding_settings_page.dart
+++ lib/views/onboarding_settings_page.dart
@@ -1,1 +1,16 @@
-// TODO: Onboarding sonrasında çalışacak ilk uygulama ayarlarını ve tanımlarını alacağımız sayfa.
+import 'package:flutter/cupertino.dart';
+
+class OnboardingSetting extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return CupertinoPageScaffold(
+        child: Center(
+      child: Column(
+        children: [
+          Text('Onboarding Setting'),
+          CupertinoButton(child: Text('Go to Home Page'), onPressed: null),
+        ],
+      ),
+    ));
+  }
+}
