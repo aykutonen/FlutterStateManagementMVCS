c6f9e05fc63b9366127a3b4e544902e91b93b3e7
aykutonen <aykutonen@gmail.com>
2020-10-18 15:57:28 +0300

    home ekranı temizlendi.

--- lib/views/home_page.dart
+++ lib/views/home_page.dart
@@ -1,7 +1,4 @@
-import 'package:StateManagementMVCS/commands/logout_command.dart';
-import 'package:StateManagementMVCS/commands/refresh_posts_command.dart';
 import 'package:StateManagementMVCS/models/app_model.dart';
-import 'package:StateManagementMVCS/models/user_model.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -15,61 +12,18 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   bool _isLoading = false;
 
-  void _handleRefreshPressed() async {
-    // Disable the RefreshBtn while the Command is running
-    setState(() => _isLoading = true);
-
-    // Run Command
-    await RefreshPostsCommand().run(context.read<AppModel>().currentUser);
-
-    // Re-enable refresh button when command is done
-    setState(() => _isLoading = false);
-  }
-
-  void _handleLogoutPressed() async {
-    setState(() => _isLoading = true);
-
-    // Run Command
-    await LogOutCommand().run();
-
-    // Re-enable logout button when command is done
-    setState(() => _isLoading = false);
-  }
-
   @override
   Widget build(BuildContext context) {
-    // Bind to UserModel.userPosts
-    final posts =
-        context.select<UserModel, List<String>>((value) => value.userPosts);
-
-    // Disable btn by removing listener when we're loading.
-    VoidCallback btnHandler = _isLoading ? null : _handleRefreshPressed;
-    VoidCallback btnLogoutHandler = _isLoading ? null : _handleLogoutPressed;
-
-    // Render list of widgets
-    final listWidgets =
-        posts != null ? posts.map((post) => Text(post)).toList() : List<Text>();
-
     return CupertinoPageScaffold(
-        child: SafeArea(
-      child: Column(
-        children: [
-          // Text(context.read<AppModel>().currentUser),
-          Flexible(
-            child: _isLoading
-                ? Center(child: CircularProgressIndicator())
-                : ListView(children: listWidgets),
-          ),
-          FlatButton(
-            child: Text('Refresh'),
-            onPressed: btnHandler,
-          ),
-          FlatButton(
-            child: Text('Log Out'),
-            onPressed: btnLogoutHandler,
-          ),
-        ],
+      child: SafeArea(
+        child: Flexible(
+            child: Center(
+          child: _isLoading
+              ? Center(child: CircularProgressIndicator())
+              : Text(
+                  'Hi, ${context.select<AppModel, String>((e) => e.currentUser)}'),
+        )),
       ),
-    ));
+    );
   }
 }
