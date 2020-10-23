ece428aad8348785286eb5da246f83f16147d6db
aykutonen <aykutonen@gmail.com>
2020-10-23 18:53:00 +0300

    su içme verilerini tutacak model düzenlendi. json desteği eklendi.

--- lib/models/drunk_model.dart
+++ lib/models/drunk_model.dart
@@ -1,7 +1,25 @@
 import 'package:StateManagementMVCS/models/unit_model.dart';
 
 class DrunkModel {
-  int amount;
-  Unit unit;
-  DateTime createDate;
+  final int amount;
+  final int unitIndex;
+  final DateTime createDate;
+
+  Unit get unit => Unit.values[unitIndex];
+
+  const DrunkModel(this.amount, this.unitIndex, this.createDate);
+
+  factory DrunkModel.fromJson(Map<String, dynamic> json) {
+    return DrunkModel(
+      json['amount'] as int,
+      json['unitIndex'] as int,
+      DateTime.parse(json['createDate'] as String),
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'amount': amount,
+        'unitIndex': unitIndex,
+        'createDate': createDate.toIso8601String(),
+      };
 }
