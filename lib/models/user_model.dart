95cfd48521619dcdee958dea18bd69af184fc9d0
aykutonen <aykutonen@gmail.com>
2020-10-23 18:53:33 +0300

    Kullanılmayan fonksiyon kaldırıldı.

--- lib/models/user_model.dart
+++ lib/models/user_model.dart
@@ -2,15 +2,8 @@ import 'package:StateManagementMVCS/models/drunk_model.dart';
 import 'package:flutter/foundation.dart';
 
 class UserModel extends ChangeNotifier {
-  List<String> _userPosts;
-  List<String> get userPosts => _userPosts;
-  set userPosts(List<String> userPosts) {
-    _userPosts = userPosts;
-    notifyListeners();
-  }
-
   // Günlük tüketim verisi...
-  List<DrunkModel> _dailyDrunks;
+  List<DrunkModel> _dailyDrunks = List<DrunkModel>();
   List<DrunkModel> get dailyDrunks => _dailyDrunks;
   set dailyDrunks(List<DrunkModel> drunks) {
     _dailyDrunks = drunks;
