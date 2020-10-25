9b88a88fd0742e57c39809512c44804e57a80863
aykutonen <aykutonen@gmail.com>
2020-10-26 01:48:12 +0300

    Name ve target alanlarının güncelleme işşemleri düzenlendi.

--- lib/views/home/pages/settings/settings.dart
+++ lib/views/home/pages/settings/settings.dart
@@ -6,9 +6,11 @@ import 'package:StateManagementMVCS/views/home/widgets/big_title.dart';
 import 'package:StateManagementMVCS/views/home/widgets/seperator.dart';
 import 'package:StateManagementMVCS/views/home/widgets/sub_title.dart';
 import 'package:StateManagementMVCS/models/hour_minute_model.dart';
+import 'package:StateManagementMVCS/models/unit_model.dart';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:provider/provider.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -18,6 +20,10 @@ class SettingsPage extends StatefulWidget {
 
 class _SettingsPageState extends State<SettingsPage> {
   final _nameInputController = TextEditingController();
+  final _targetInputController = TextEditingController();
+  final _targetFocusNode = FocusNode();
+  final _nameFocusNode = FocusNode();
+
   String _error = "";
   Duration _wakeup;
   Duration _sleep;
@@ -31,17 +37,39 @@ class _SettingsPageState extends State<SettingsPage> {
       _wakeup = Duration(hours: w.hour, minutes: w.minute);
       _sleep = Duration(hours: s.hour, minutes: s.minute);
     });
+    _targetFocusNode.addListener(_handleTargetSaveButton);
+    _nameFocusNode.addListener(_handleNameFocusChange);
 
     super.initState();
   }
 
-  void _handleSaveButton() async {
+  void _handleNameFocusChange() async {
+    if (_nameFocusNode.hasFocus) return;
+
     if (_nameInputController.text.isEmpty) {
       setState(() => _error = "İsim zorunlu");
+      _nameFocusNode.requestFocus();
     } else {
       setState(() => _error = "");
       await AppCommand().saveUsername(_nameInputController.text);
-      FocusScope.of(context).unfocus();
+    }
+  }
+
+  void _handleTargetSaveButton() async {
+    if (_targetFocusNode.hasFocus) return;
+
+    if (_targetInputController.text.isEmpty) {
+      setState(() => _error = "Target zorunlu");
+      _targetFocusNode.requestFocus();
+    } else {
+      setState(() => _error = "");
+      var amount = int.tryParse(_targetInputController.text);
+      if (amount == null || amount == 0) {
+        setState(() => _error = "Geçerisz bir miktar girildi.");
+        _targetFocusNode.requestFocus();
+      } else {
+        await AppCommand().setTargetAmount(amount);
+      }
     }
   }
 
@@ -73,6 +101,10 @@ class _SettingsPageState extends State<SettingsPage> {
     _nameInputController.selection = TextSelection.fromPosition(
         TextPosition(offset: _nameInputController.text.length));
 
+    _targetInputController.text = model.targetAmount.toString();
+    _targetInputController.selection = TextSelection.fromPosition(
+        TextPosition(offset: _targetInputController.text.length));
+
     return GestureDetector(
       behavior: HitTestBehavior.opaque,
       onTap: FocusScope.of(context).unfocus,
@@ -100,8 +132,8 @@ class _SettingsPageState extends State<SettingsPage> {
                       textCapitalization: TextCapitalization.sentences,
                       controller: _nameInputController,
                       textAlign: TextAlign.right,
-                      onEditingComplete: _handleSaveButton,
                       textInputAction: TextInputAction.done,
+                      focusNode: _nameFocusNode,
                     ),
                   ),
                 ),
@@ -163,9 +195,24 @@ class _SettingsPageState extends State<SettingsPage> {
               mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: [
                 Text('Target'),
-                CupertinoButton(
-                  child: Text('Değer 3'),
-                  onPressed: null,
+                Expanded(
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 100),
+                    child: CupertinoTextField(
+                      maxLines: 1,
+                      controller: _targetInputController,
+                      keyboardType: TextInputType.number,
+                      inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                      textAlign: TextAlign.right,
+                      onEditingComplete: _handleTargetSaveButton,
+                      textInputAction: TextInputAction.done,
+                      focusNode: _targetFocusNode,
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(left: 10.0),
+                  child: Text(model.unit.name),
                 ),
               ],
             ),
