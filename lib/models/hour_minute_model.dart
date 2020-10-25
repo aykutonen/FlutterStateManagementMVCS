f1382ac1e08db532b0d26ca995dddaa4bde897db
aykutonen <aykutonen@gmail.com>
2020-10-25 21:15:34 +0300

    HourMinute modelin String'e çevirme fonksiyonu düzenlendi.

--- lib/models/hour_minute_model.dart
+++ lib/models/hour_minute_model.dart
@@ -15,5 +15,9 @@ class HourMinute {
 }
 
 extension HourMinuteExtension on HourMinute {
-  String get toDisplayString => '${this.hour}:${this.minute}';
+  String get toDisplayString {
+    var h = this.hour > 9 ? this.hour : "0${this.hour}";
+    var m = this.minute > 9 ? this.minute : "0${this.minute}";
+    return '$h:$m';
+  }
 }
